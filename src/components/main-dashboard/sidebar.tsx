d7d53367980901ac5036ee44f08f3a232ff6137d
jazzygee36 <jazzygee36@gmail.com>
2025-06-05 17:43:42 +0100

    sidebar

--- src/components/main-dashboard/sidebar.tsx
+++ src/components/main-dashboard/sidebar.tsx
@@ -54,7 +54,7 @@ const Sidebar = ({
       )}
       {/* Sidebar Container */}
       <div
-        className={`pl-3 md:pl-[30px] lg:pl-[40px] fixed lg:static top-0 left-0 h-[100vw] w-[60%] md:w-[30%] lg:w-[19%] bg-[#ffffff]  transition-transform duration-300 ease-in-out z-20
+        className={`pl-3 md:pl-[30px] lg:pl-[40px] fixed lg:static top-0 left-0 h-full  w-[60%] md:w-[30%] lg:w-[19%] bg-[#ffffff]  transition-transform duration-300 ease-in-out z-20
         ${isOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0`}
       >
         {/* Logo & Close Button */}
